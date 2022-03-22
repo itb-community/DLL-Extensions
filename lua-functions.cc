2b40710cdbffbaebea802a82a217ddb5e45e0fa9
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2022-03-22 17:44:10 +0100

    Fix crash when function argument is not a string

--- lua-functions.cc
+++ lua-functions.cc
@@ -14,17 +14,23 @@ using namespace luabridge;
 #include <regex>
 
 int listDirectoryFull(lua_State *L, int mode) {
-	const char *dirname = lua_tostring(L, 1);
-
 	lua_newtable(L);
+
+	if (!lua_isstring(L, 1))
+		return 1;
+
+	const char *dirname = lua_tostring(L, 1);
 
 	WIN32_FIND_DATAA fdFile;
 	HANDLE handle = NULL;
+	std::regex fullPath("(.*)");
 	std::regex startsWithDriveLetter("^.:.*$");
-	std::string path = format("%s\\*.*", dirname);
+	std::string path;
 
-	if (!std::regex_match(path, startsWithDriveLetter))
-		path = format(".\\%s", path);
+	if (!std::regex_match(dirname, startsWithDriveLetter))
+		path = regex_replace(dirname, fullPath, ".\\$1\\*.*");
+	else
+		path = regex_replace(dirname, fullPath, "$1\\*.*");
 		
 	if((handle = FindFirstFileA(path.c_str(), &fdFile)) == INVALID_HANDLE_VALUE) {
 		return 1;
